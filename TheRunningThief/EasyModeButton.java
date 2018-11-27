11c144c23e49b796c97a771778166327e85155ce
Min Lu <37051693+luminous66@users.noreply.github.com>
2018-11-27 12:18:51 -0800

    Add Arrow to indicate choosing Easy or Hard Mode

--- TheRunningThief/EasyModeButton.java
+++ TheRunningThief/EasyModeButton.java
@@ -7,21 +7,16 @@ import greenfoot.*; // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @version (a version number or a date)
  */
 public class EasyModeButton extends Actor{
-	GreenfootSound playGameMusic = new GreenfootSound("easyhardmodebutton.mp3");
 	/**
 	 * Act - do whatever the EasyModeButton wants to do. This method is called
 	 * whenever the 'Act' or 'Run' button gets pressed in the environment.
 	 */
 	
 	public void act() {
-		if (Greenfoot.mousePressed(this)) {
-			GreenfootImage img = getImage();
-			img.scale(200, 55);
-		}
+		
 
 		if (Greenfoot.mouseClicked(this)) {
 			GameModeSingleton.getInstance().setStrategy(new EasyStrategy());
-			playGameMusic.play();
 		}
 	}
 }
