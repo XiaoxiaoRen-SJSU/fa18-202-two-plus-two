fd221f50f9c2e348a3171f16451427a4327ca184
Yimu Yang <yimuyang19911223@gmail.com>
2018-11-27 17:14:03 -0800

    Game Mode Bug Fixed

--- TheRunningThief/HomePage.java
+++ TheRunningThief/HomePage.java
@@ -7,60 +7,62 @@ import greenfoot.*; // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @version (a version number or a date)
  */
 public class HomePage extends World {
-	Thief thief;
-	WorldController worldCtrl;
-	private IScreenCommand playGame;
+    Thief thief;
+    WorldController worldCtrl;
+    private IScreenCommand playGame;
 
-	// Create by Xiaoxiao Ren, the IScreenCommand variable.
-	/**
-	 * Constructor for objects of class page.
-	 * 
-	 */
-	public HomePage() {
-		// Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-		super(1200, 780, 1);
+    // Create by Xiaoxiao Ren, the IScreenCommand variable.
+    /**
+     * Constructor for objects of class page.
+     * 
+     */
+    public HomePage() {
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(1200, 780, 1);
 
-		// Create by Xiaoxiao Ren, initial commmand parttern.
-		playGame = new ScreenCommand();
-		setReceiverPlayGame();
-		worldCtrl = new WorldController();
-		thief = Thief.getThief();
-		prepare();
-	}
+        // Create by Xiaoxiao Ren, initial commmand parttern.
+        playGame = new ScreenCommand();
+        setReceiverPlayGame();
+        worldCtrl = new WorldController();
+        thief = Thief.getThief();
+        prepare();
+    }
 
-	// Create by Xiaoxiao Ren. Prepare home page.
-	private void prepare() {
-		PlayGameButton playGameButton = new PlayGameButton();
-		addObject(playGameButton, 1003, 675);
-		playGameButton.setScreenItem(playGame);
+    // Create by Xiaoxiao Ren. Prepare home page.
+    private void prepare() {
+        PlayGameButton playGameButton = new PlayGameButton();
+        addObject(playGameButton, 1003, 675);
+        playGameButton.setScreenItem(playGame);
 
-		EasyModeButton easyModeButton = new EasyModeButton();
-		addObject(easyModeButton, 309, 686);
-		HardModeButton hardModeButton = new HardModeButton();
-		addObject(hardModeButton, 509, 686);
+        EasyModeButton easyModeButton = new EasyModeButton();
+        addObject(easyModeButton, 309, 686);
+        HardModeButton hardModeButton = new HardModeButton();
+        addObject(hardModeButton, 509, 686);
 
-		Arrow arrow = new Arrow();
-		addObject(arrow, 309, 745);
+        Arrow arrow = new Arrow();
+        addObject(arrow, 309, 745);
+        // Mode bug fixed by Yimu Yang
+        GameModeSingleton.getInstance().setStrategy(new EasyStrategy());
+        
+        Title title = new Title();
+        addObject(title, 812, 127);
 
-		Title title = new Title();
-		addObject(title, 812, 127);
+        thief.countOfDiamond = 0;
+        thief.lives = 3;
 
-		thief.countOfDiamond = 0;
-		thief.lives = 3;
+        thief.setWorldController(worldCtrl);
+    }
 
-		thief.setWorldController(worldCtrl);
-	}
-
-	// Create by Xiaoxiao Ren. Set Command receiver.
-	private void setReceiverPlayGame() {
-		playGame.setReceiver(new IScreenReceiver() {
-			/** Command Action */
-			public void doAction() {
-				GamePage beginPage = worldCtrl.getPageChain().get(0);
-				beginPage.prepare();
-				Greenfoot.setWorld(beginPage);
-			}
-		});
-	}
+    // Create by Xiaoxiao Ren. Set Command receiver.
+    private void setReceiverPlayGame() {
+        playGame.setReceiver(new IScreenReceiver() {
+            /** Command Action */
+            public void doAction() {
+                GamePage beginPage = worldCtrl.getPageChain().get(0);
+                beginPage.prepare();
+                Greenfoot.setWorld(beginPage);
+            }
+        });
+    }
 
 }
